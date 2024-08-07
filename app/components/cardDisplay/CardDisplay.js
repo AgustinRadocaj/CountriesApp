f9886b8ef40381bf252bcde81a8af7b7c1318c02
Agustin Radocaj <jaguscaj@gmail.com>
2024-08-07 18:30:57 -0300

    search bar functionality

--- app/components/cardDisplay/CardDisplay.js
+++ app/components/cardDisplay/CardDisplay.js
@@ -5,19 +5,32 @@ import Card from "../card/Card"
 const cardDisplay = () => {
 
     const [countries, setCountries] = useState([]);
+    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const searchHandler = (event) => {
+        const searchTerm = event.target.value;
+        setSearch(searchTerm);
+        const filtered = countries.filter((country) => 
+            country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredCountries(filtered);
+    };
 
     useEffect(() => {
-         const fetchData = async () => {
-            await fetch('https://restcountries.com/v3.1/all')
-            .then(response => response.json())
-            .then(data => setCountries(data))
-        }
-         fetchData()
-    }, [])
+        const fetchData = async () => {
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            const data = await response.json();
+            setCountries(data);
+            setFilteredCountries(data);
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
         <div className="flex justify-between h-20 mb-9 p-5">
-            <input type="text"placeholder="Search for a country..." />
+            <input type="text"placeholder="Search for a country..." value={search} onChange={searchHandler}/>
         <select>
             <option value="Africa">Africa</option>
             <option value="Antartic">Antartic</option>
@@ -28,7 +41,7 @@ const cardDisplay = () => {
         </select>
         </div>
         <div className=" w-full h-auto px-10 grid grid-cols-4 gap-y-16 justify-items-center items-center">
-        {countries.slice(0, 8).map((countrie, index) => 
+        {filteredCountries.slice(0, 8).map((countrie, index) => 
                     <Card 
                     key={index}
                     id={countrie.cca3}
