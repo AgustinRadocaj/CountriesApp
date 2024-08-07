c93b2f1b4d95c6b66134a6a8967502cf26b0c91e
Agustin Radocaj <jaguscaj@gmail.com>
2024-08-07 19:13:48 -0300

    region filter - alphabetic filter

--- app/components/cardDisplay/CardDisplay.js
+++ app/components/cardDisplay/CardDisplay.js
@@ -7,6 +7,8 @@ const cardDisplay = () => {
     const [countries, setCountries] = useState([]);
     const [filteredCountries, setFilteredCountries] = useState([]);
     const [search, setSearch] = useState("");
+    const [region, setRegion] = useState("All");
+    const [alfa, setAlfa] = useState("S");
 
     const searchHandler = (event) => {
         const searchTerm = event.target.value;
@@ -16,6 +18,28 @@ const cardDisplay = () => {
         );
         setFilteredCountries(filtered);
     };
+
+    const regionHandler = (event) => {
+        const selectedRegion = event.target.value;
+        setRegion(event.target.value);
+        if (selectedRegion === "All") {
+            setFilteredCountries(countries);
+        } else {
+            const filtered = countries.filter((country) => country.region === selectedRegion);
+            setFilteredCountries(filtered);
+        }
+    };
+
+    
+    const alfaHandle = (event) => {
+        const alfaValue = event.target.value;
+        setAlfa(alfaValue);
+
+        if (alfaValue === "S") setFilteredCountries(countries);
+        if (alfaValue === "A") setFilteredCountries(countries.sort((a, b) => a.name.common.localeCompare(b.name.common)));
+        if (alfaValue === "D") setFilteredCountries(countries.sort((a, b) => b.name.common.localeCompare(a.name.common)));
+    }
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,7 +55,13 @@ const cardDisplay = () => {
         <>
         <div className="flex justify-between h-20 mb-9 p-5">
             <input type="text"placeholder="Search for a country..." value={search} onChange={searchHandler}/>
-        <select>
+        <select value={alfa} onChange={alfaHandle}>
+            <option value="S">Alfabetico</option>
+            <option value="A">A-Z</option>
+            <option value="D">Z-A</option>
+        </select>
+        <select value={region} onChange={regionHandler}>
+            <option value="All">All</option>
             <option value="Africa">Africa</option>
             <option value="Antartic">Antartic</option>
             <option value="Americas">Americas</option>
