b495675831e36eb7638ec69fa27910f1b8ed338d
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-11 16:42:42 -0300

    navbar name change

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
   render() {
     return (
      <div className='flex justify-between h-20 mb-8 shadow-lg p-5'>
-        <h1 className='text-2xl'>Where in the world?</h1>
+        <h1 className='text-2xl'>Worldpedia</h1>
         <button className='hidden'>toggle theme</button>
      </div>
     )
